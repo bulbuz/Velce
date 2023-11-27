747930ec62a51dbe0fb9dc4eab97bb31e87a3285
Vilgot Johansen <vilgot.johansen@edu.orebro.se>
2023-11-27 16:33:29 +0100

    Level 1
    - added level 1 in world 1
    - added game object to switch scenes and shit code

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
