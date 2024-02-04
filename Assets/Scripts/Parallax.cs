d83216a7487bd1e31b407e292b7375aec5b94eb3
ToglivvilgoT <vilgot.johansen@hotmail.com>
2024-02-04 12:25:52 +0100

    Parallax done
    - added middle-ground parallax elements.
    - decorated the scene with parallax elements
    - added parallax script
    - changed pixel-perfect cam from 16 pixels to 64 pixels wich allows movement of 1/4 of a pixel wich is a bit smoother and needed so parallax doesnt look like shit

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  
+
+public class Parallax : MonoBehaviour
+{
+    public Camera cam;
+
+    //backgroundZ determines at what Z the parallax effect will be 100%, i.e. it follows the camera perfectly.
+    private const float backgroundZ = -16f;
+
+    private Vector2 origin;
+    private float parallaxAmount;
+
+    void Start()
+    {
+        origin = (Vector2)transform.position;
+        // all objects are set to have whole number coordinates so they are synced up. If this is skipped and a
+        // pixel-perfect camera is used, all parallax objects will move at differnet times wich is ugly
+        origin.x = Mathf.Round(origin.x);
+        origin.y = Mathf.Round(origin.y);
+        parallaxAmount = transform.position.z * (1 / backgroundZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new(cam.transform.position.x - origin.x, cam.transform.position.y - origin.y);
+        offset *= parallaxAmount;
+        transform.position = offset + origin;
+    }
+}
